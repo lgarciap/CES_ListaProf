0e99bc4c77a25dd526a44222fc6148a2f2d8e911
Lynette <lgarciap@uvg.edu.gt>
2017-03-09 10:00:27 -0600

    principal enlazada

--- ListaSimplementeEnlazada/src/Principal.java
+++ ListaSimplementeEnlazada/src/Principal.java
@@ -1,3 +1,5 @@
+import java.text.Normalizer.Form;
+
 /**
  * 
  */
@@ -6,6 +8,7 @@
  * @author flaquitqm
  *
  */
+
 public class Principal {
 
 	/**
@@ -13,7 +16,22 @@ public class Principal {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		ListaSecuencial<Integer> lista = new ListaSecuencial<Integer>(10);
+		int tamanio = lista.getTamanio();
+		lista.addFirst(5);
+		lista.addFirst(9);
+		lista.addFirst(4);
+		lista.addFirst(6);
+		lista.addFirst(8);
+		lista.addFirst(7);
+		int value = 25;
+		int indice = 8;
+		lista.add(indice,value);
+		
+		lista.removeFirst();
+			System.out.println(lista.get(indice));
 	}
+	
+	
 
 }
